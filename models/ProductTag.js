7433ba9f4deeb22a0e494e1142ed3d134ab764dd
unknown <nukasine@gmail.com>
2021-04-18 09:03:00 -0600

    set up framework for the models. Still needs some validation and references written in, but the basic structure is now present

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    // need to write references to this model's id
+    product_id: {
+      type: DataTypes.INTEGER,
+    },
+    // needs to reference tag model's id
+    tag_id: {
+      type: DataTypes.INTEGER,
+    },
   },
   {
     sequelize,
